4a9bc8efd8b5fbe1eb96bb3d5cabf373cb27a8b2
ThirteenAsh <1984885454@qq.com>
2026-08-17 16:07:35 +0800

    初始化项目：搭建 Tauri + Vue + TypeScript 应用骨架

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main />
+</template>
+
+<style>
+:root {
+  font-family: Arial, sans-serif;
+  color: #0f0f0f;
+  background: #f6f6f6;
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  margin: 0;
+}
+</style>
